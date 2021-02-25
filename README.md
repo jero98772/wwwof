7b96e364f154c1955bb6b9e432c9674aed0b5503
jero98772 <jero98772@protonmail.com>
2021-02-24 21:25:52 -0500

    fix setup.py and update readme

--- README.md
+++ README.md
@@ -1,10 +1,37 @@
 # wwwofish
 word wide web of fish
-word wide web of fish , wwwwofish or  wwwof is a set of proyects of fish
+word wide web of fish , wwwwofish or  wwwof is a set of proyects of fish,
+word wide web of fishes is a webpage with web interface for drawfishtank , divepc, calcuph,curapeces and others 
+
+# fishes and code 
+are an infinite mix of [ideas](https://jero98772.pythonanywhere.com/wwwof/fishproyectsEN.html ) and projects. with fishes
+
 # look of proyect
 https://www.youtube.com/watch?v=WsMlSTcJ_bk
 image datasets
 https://drive.google.com/drive/folders/1VAtgS7fqS8MqXprYLcX69QLdEWNu8mdv?usp=sharing
+[visit us](https://jero98772.pythonanywhere.com/wwwof/ )
+
+# proyects in wwwof
+![wwwof](http://wiki.unloquer.org/_media/personas/jero98772/hackerpezpezcamisa.png)
+### drawfishtank
+[here](https://jero98772.pythonanywhere.com/wwwof/drawFISHTANK.html)
+
+<img src="http://wiki.unloquer.org/_media/personas/jero98772/2021-02-24-145222_225x175_scrot.png" alt="drawfishtnak" width="200" height="200"/>
+
+### divePC (not is software)
+[here](https://jero98772.pythonanywhere.com/wwwof/divePC.html)
+
+<img src="http://wiki.unloquer.org/_media/personas/jero98772/divepc2.png" alt="divepc" width="200" height="200"/>
+
+### CalcupH 
+[here](https://jero98772.pythonanywhere.com/wwwof/calcupH.html)
+
+<img src="http://wiki.unloquer.org/_media/personas/jero98772/calcuph.png" alt="calcuph" width="200" height="200"/>
+
+### Curapeces
+[here](https://jero98772.pythonanywhere.com/wwwof/curapeces.html),is not working is very heavy for host but have strong AI , some times is working with "hack" for run in my machine is proyect for identify fish diases [code](https://github.com/jero98772/curapeces)
+![curapeces](http://wiki.unloquer.org/_media/personas/jero98772/curapeces.png)
 # you can inlcuide in your web change the variable webpage
 ### contributes with donations
 	
--- setup.py
+++ setup.py
@@ -1,39 +1,20 @@
-try:
-#dont working
-    import subprocess
-    try:
-        import time
-        import psycopg2, psycopg2.extras
-        from flask_sqlalchemy import SQLAlchemy
-        from flask import Flask, render_template, request, flash, redirect 
-        import urllib.request
-        import numpy as np
-        import tensorflow as tf
-        import cv2
-        import os
-        from tensorflow.python.keras import backend as K
-        import tensorflow
-    except:
-        install_pip=str("sudo pip install -U urllib3 tensorflow opencv-python  numpy Flask psycopg2 keras flask_sqlalchemy" )
-        subprocess.run(install_pip , shell=True)
-except:
-    print("run to install setup you need install dependes can install automatic pip install subprocess.run  and run again python3 setup.py")
-"""
-
-
 from setuptools import setup, find_packages
-
 setup(
     name='wwwof',
-    version='1.3',
+    version='2.7.0',
     license='GPLv3',
     author_email='jero98772@protonmail.com',
     author='jero98772',
-    description='proyects about fish',
-    url='http://127.0.0.1:5000/',
+    description='wwwof , word wide web of fishes is a webpage with web interface for drawfishtank , divepc, calcuph,curapeces and others ',
+    url='https://jero98772.pythonanywhere.com/',
     packages=find_packages(),
-    install_requires=['urllib3 == 1.25.7', 'tensorflow == 2.0.0', 'opencv-python==4.1.2 ', 'numpy==1.18.0', 'Flask==1.1.1', 'psycopg2==2.8.4' ,'keras==2.3.1,' 'flask_sqlalchemy==2.4.1','subprocess.run==17.1.1'],
+    install_requires=['Flask', 'tensorflow', 'numpy', 'opencv-python', 'removebg'],
     include_package_data=True,
-      zip_safe=False
-)
-"""
+    )
+print(" _ __   ___ | |_ ___  ___ ")
+print("| '_ \ / _ \| __/ _ \/ __|")
+print("| | | | (_) | ||  __/\__ \ ")
+print("|_| |_|\___/ \__\___||___/")
+print("\n\n","PLEASE UPDATE PIP with (if needed):","\n\n","> pip intsall -U pip")
+print("\n\n","SOME COMONS ERRORS of installing","\n\n","> tensorflow version == 1.14.0 \n> conda enviroment for tensorflow ")
+#print("tensorflow not required in general , curapeces need tensorflow")
