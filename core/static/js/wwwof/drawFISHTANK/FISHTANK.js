1243ee347801222e3e7cab8c57368a172b8e322d
jero98772 <jero98772@protonmail.com>
2021-03-02 17:28:36 -0500

    update drawfishtank

--- core/static/js/wwwof/drawFISHTANK/FISHTANK.js
+++ core/static/js/wwwof/drawFISHTANK/FISHTANK.js
@@ -1,97 +1,73 @@
 class fishtank{
 	constructor() {
 		this.figura=document.getElementById("figura");
+		this.litrostxt=document.getElementById('litros');
 		this.texto_lado1=document.getElementById("lado01");
 		this.texto_lado2=document.getElementById("lado02");
 		this.texto_lado3=document.getElementById("lado03");
 		this.entrada_lado_1=document.getElementById("entrada_lado_1");
 		this.entrada_lado_2=document.getElementById("entrada_lado_2");
 		this.entrada_lado_3=document.getElementById("entrada_lado_3");
-		this.parrafo=document.getElementById('parrafo');
 		this.vel =0.01
 		this.lados = new createVector(this.texto_lado1,this.texto_lado2,this.texto_lado3)
 		this.frameCount = 0;
 		this.parrafo;
 	}
 rectangle(){
-
-	this.l = parseFloat(this.entrada_lado_1.value);
-	this.w = parseFloat(this.entrada_lado_2.value);
-	this.h = parseFloat(this.entrada_lado_3.value);
-	this.litros=(this.l*this.h*this.w)/1000;
-	console.log(this.litros);
-	//this.entrada = String(this.entrada)
-	//this.litros = String(this.litros)
-	this.parrafo.innerHTML=this.litros+"  l";
 	this.figura.innerHTML='rectagle';
-	this.lados.x.innerHTML='large cm';
-	this.lados.y.innerHTML='heigth cm';
-	this.lados.z.innerHTML='width cm';
+	document.getElementById('help').innerHTML = ""
+	litros=(this.entrada_lado_1.value*this.entrada_lado_2.value*this.entrada_lado_3.value)/1000;
+	this.litrostxt.innerHTML=litros+"  l";
+	this.texto_lado1.innerHTML='large cm';
+	this.texto_lado2.innerHTML='heigth cm';
+	this.texto_lado3.innerHTML='width cm';
 	this.frameCount = this.frameCount+ this.vel;
 	rotateX(this.frameCount);
   	rotateY(this.frameCount);
-	this.fishtank = box(this.l,this.w,this.h,0,0);
-	//return this.fishtank
+	this.fishtank = box(this.entrada_lado_1.value ,this.entrada_lado_2.value ,this.entrada_lado_3.value ,0,0);
 	}	
 cubic(){
-	this.l=parseFloat(this.entrada_lado_1.value)
-	this.litros=(this.l**3)/1000;
-
-	console.log(this.litros);
-	//this.entrada = String(this.entrada)
-	//this.litros = String(this.litros)
-	this.parrafo.innerHTML=this.litros+" l"
 	this.figura.innerHTML='cubic'
-	this.lados.x.innerHTML='large (solamente) cm '
-	this.lados.y.innerHTML='not needed'
-	this.lados.z.innerHTML='not needed'
+	document.getElementById('help').innerHTML = ""
+	litros=(parseFloat(this.entrada_lado_1.value)**3)/1000;
+	this.litrostxt.innerHTML=litros+" l"
+	this.texto_lado1.innerHTML='large cm '
+	this.texto_lado2.innerHTML='--NOT NEEDED--'
+	this.texto_lado3.innerHTML='--NOT NEEDED--'
 	this.frameCount = this.frameCount+ this.vel;
-	rotateX(this.frameCount + 1 );
-	rotateY(this.frameCount + 1 );
+	rotateX(this.frameCount);
+	rotateY(this.frameCount);
 	this.fishtank = box(this.l);
-	//return this.fishtank
 }
 
 triangularpris(){
-	this.b=parseFloat(this.entrada_lado_1.value)
-	this.Hbase=parseFloat(this.entrada_lado_2.value)
-	this.h=parseFloat(this.entrada_lado_3.value)
-
-	this.litros=((this.b*this.Hbase)/2)*this.h
-	this.litros=	this.litros/1000
-	//this.entrada = String(this.entrada)
-	//this.litros = String(this.litros)
+	litros=(((this.entrada_lado_1.value*this.entrada_lado_2.value)/2)*this.entrada_lado_3.value)/1000
 	this.figura.innerHTML='trianglar prism'
-	this.parrafo.innerHTML=this.litros+"l"
-	this.lados.x.innerHTML='large of base cm'
-	this.lados.y.innerHTML='heigth of the base cm'
-	this.lados.z.innerHTML='heigth of form cm '
-	console.log(this.litros);
+	this.litrostxt.innerHTML=litros+"l"
+	this.texto_lado1.innerHTML='large of base cm'
+	this.texto_lado2.innerHTML='heigth of base cm'
+	this.texto_lado3.innerHTML='heigth  cm '
 	this.frameCount = this.frameCount+ this.vel;
-
-	rotateX(this.frameCount + 1 );
-  	rotateY(this.frameCount + 1 );	
+	document.getElementById('help').innerHTML = 'help links <br><button value="trianglar prism"><a href="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Faulaprende.com%2Fwp-content%2Fuploads%2F2019%2F01%2Fprisma-triangular.jpg&f=1&nofb=1"> trianglar prism </a></button><br><button value="base"><a href="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmatematicabasica.net%2Fwp-content%2Fuploads%2F2019%2F02%2Farea-do-triangulo-3.png&f=1&nofb=1">base</a></button>';
+	rotateX(this.frameCount);
+  	rotateY(this.frameCount);	
 	this.fishtank =  cylinder(this.b,this.h,3)
 	//return this.fishtank
 }
 cilinder(){
-	this.R=parseFloat(this.entrada_lado_1.value)
-	this.h=parseFloat(this.entrada_lado_2.value)
-	this.litros=PI*(this.R**2)*this.h
-	this.litros=	this.litros/1000
-	//this.entrada = String(this.entrada)
-	//this.litros = String(this.litros)
-	this.lados.x.innerHTML='radio cm'
-	this.lados.y.innerHTML='heigth cm'
-	this.lados.z.innerHTML='not needed'
-	this.parrafo.innerHTML=this.litros+" l"
+	this.figura.innerHTML='cilinder';
+	document.getElementById('help').innerHTML = ""
+	//this.R=parseFloat(this.entrada_lado_1.value)
+	//this.h=parseFloat(this.entrada_lado_2.value)
+	litros=PI*(parseFloat(this.entrada_lado_1.value)**2)*parseFloat(this.entrada_lado_2.value)/1000	
+	this.texto_lado1.innerHTML='radio cm'
+	this.texto_lado2.innerHTML='heigth cm'
+	this.texto_lado3.innerHTML='--NOT NEEDED--'
+	this.litrostxt.innerHTML=litros+" l"
 	this.frameCount = this.frameCount+ this.vel;
-
-	console.log(this.litros);
-	rotateX(this.frameCount  );
-  	rotateY(this.frameCount );
+	rotateX(this.frameCount);
+  	rotateY(this.frameCount);
 	this.fishtank =  cylinder(this.R,this.h,24)
-	//return this.fishtank
 }
 
 
--- core/static/js/wwwof/drawFISHTANK/drawFISHTANK.js
+++ core/static/js/wwwof/drawFISHTANK/drawFISHTANK.js
@@ -1,18 +1,15 @@
 let dft ;
-
 function setup() {
-	canvas = createCanvas(350, 350, WEBGL) ;
+	canvas = createCanvas(750, 463, WEBGL) ;
 	dft = new fishtank();
 }
 function draw() {
 	background(150);
-	x = (windowWidth - width) / 2;
-	y = 215;
+	x = (windowWidth - width) / 2 ;
+	y = 300;
 	canvas.position(x, y);
-	Select = document.From1.Selecion;
-	Select=document.getElementById('Selecion1');
+	Select = document.getElementById('select');
 	seleccion = Select.options[Select.selectedIndex].value;
-	console.log(seleccion)
 	if (seleccion == 'rectagle') {
 		dft.rectangle()
 	}
